9b9e78a4403fb2da42b0ca2fbe193ecd51bf8d93
Kanan Farzali <kanan.farzali@gmail.com>
2024-02-29 13:34:12 +0100

    create searcg result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 
 export default async function WebSearchPage({ searchParams }) {
@@ -26,7 +27,7 @@ export default async function WebSearchPage({ searchParams }) {
 
   return (
     <div>
-      {results && results.map((result) => <h1 key={result}>{result.title}</h1>)}
+      {results && <WebSearchResults results={data} />}
     </div>
   );
 }
